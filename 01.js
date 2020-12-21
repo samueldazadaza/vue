2c587278fa9f98cc211af4a1df0dc7b761c30eef
Samuel Daza <samueldazadaza@gmail.com>
2020-12-21 00:45:29 -0500

    apreandiendo a crear objetos, esta dificil de nentender, pero vamos avanzando

---
+++ 01.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app', //dentro de este id se trabajara con vue
+    data: { //abrir otro objeto
+        titulo: 'hola mundo con vue',
+        frutas: [
+            alimento: 'manzana' = 10, 
+            alimento: 'pera' = 3,
+            alimento: 'uvas' = 5,
+        ],
+    }
+})
