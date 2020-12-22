474e41e50bdc5b02bdcff9d32f6f010210ae8adf
Samuel Daza <samueldazadaza@gmail.com>
2020-12-22 00:38:22 -0500

    agregar total de 3 items

--- 01.js
+++ 01.js
@@ -7,17 +7,26 @@ const app = new Vue({
             {nombre:'cisco', cantidad:0},
             {nombre:'huawei', cantidad:11},
         ],
-        nuevoequipo: ''
+        nuevoequipo: '',
+        total: 0
     },
     //creando metodos
     methods:{
         agregarequipo () {
             this.equipos.push({ //this = para accedes al data
-                nombre: this.nuevoequipo,
-                cantidad: this.nuevoequipo, //trae datos desde input desde html
+                nombre: this.nuevoequipo, cantidad:0 //trae datos desde input desde html
             });
             this.nuevoequipo = ''; // para borrar texto pegote de html del input
-        },
+        }
         
+    },
+    computed:{
+        sumarequipos(){
+            this.total = 0;
+            for(item of this.equipos){
+                this.total = this.total + item.cantidad;
+            }
+            return this.total;
+        }
     }
 })
