45fa2ec0a3678909291709559a9ea3ea13e08b26
Samuel Daza <samueldazadaza@gmail.com>
2020-12-21 23:52:41 -0500

    creando interaccion desde html hacia js

--- 01.js
+++ 01.js
@@ -2,10 +2,19 @@ const app = new Vue({
     el: '#app', //dentro de este id se trabajara con vue
     data: { //abrir otro objeto
         titulo: 'hola mundo con vue',
-        frutas: [ //contruyendo objetos dentro de array dentro de llaves
-            {nombre:'pera', cantidad:10},
-            {nombre:'manzana', cantidad:0},
-            {nombre:'platano', cantidad:11},
+        equipos: [ //contruyendo objetos dentro de array dentro de llaves
+            {nombre:'kaon', cantidad:10},
+            {nombre:'cisco', cantidad:0},
+            {nombre:'huawei', cantidad:11},
         ],
+        nuevoequipo: ''
+    },
+    //creando metodos
+    methods:{
+        agregarequipo () {
+            this.equipos.push({ //this = para accedes al data
+                nombre: this.nuevoequipo, cantidad:0,
+            })
+        }
     }
 })
