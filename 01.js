42d076d40400e712711620d002c73287d133b2d7
Samuel Daza <samueldazadaza@gmail.com>
2020-12-22 23:58:41 -0500

    cambiando diseño de web

--- 01.js
+++ 01.js
@@ -1,11 +1,11 @@
 const app = new Vue({
     el: '#app', //dentro de este id se trabajara con vue
     data: { //abrir otro objeto
-        titulo: 'hola mundo con vue',
+        titulo: 'Aplicativo para crear inventario en empresa',
         equipos: [ //contruyendo objetos dentro de array dentro de llaves
-            {nombre:'kaon', cantidad:10},
-            {nombre:'cisco', cantidad:0},
-            {nombre:'huawei', cantidad:11},
+            {nombre:'Laptops', cantidad:10},
+            {nombre:'Mesas', cantidad:0},
+            {nombre:'Sillas', cantidad:11},
         ],
         nuevoequipo: '',
         total: 0
